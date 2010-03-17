3dea1749cf669d394ee0c3b002f9f5fc81234018
Łukasz Langa <lukasz@langa.pl>
2010-03-17 02:52:23 +0100

    forgot to bump version in setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup, find_packages
 
 setup (
     name = 'langacore.kit.common',
-    version = '0.1.4',
+    version = '0.1.5',
     author = 'LangaCore, Lukasz Langa',
     author_email = 'support@langacore.org, lukasz@langa.pl',
     description = "A library of various simple common routines that keep being " \
