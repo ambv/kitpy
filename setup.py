98eba3be8b91c72f0bf6ee32b5b10bd10bb42845
Łukasz Langa <lukasz@langa.pl>
2010-07-21 00:20:26 +0100

    updates to metadata

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup (
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ]
     )
