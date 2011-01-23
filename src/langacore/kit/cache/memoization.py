6ed208e50517f5a39774f43465954c2dcc02f072
Łukasz Langa <lukasz@langa.pl>
2011-01-23 21:46:55 +0100

    trailing whitespace removed

--- src/langacore/kit/cache/memoization.py
+++ src/langacore/kit/cache/memoization.py
@@ -35,21 +35,21 @@ import cPickle as pickle
 import sys
 from time import time
 from functools import wraps
- 
+
 
 def memoize(func=None, update_interval=300, max_size=256, skip_first=False, fast_updates=False):
     """Memoization decorator.
-            
+
         :param update_interval: time in seconds after which the actual function
-                                will be called again 
+                                will be called again
 
         :param max_size: maximum buffer count for distinct memoize hashes for
-                         the function 
-        
+                         the function
+
         :param skip_first: ``False`` by default; if ``True``, the first
                            argument to the actual function won't be added to
-                           the memoize hash 
-        
+                           the memoize hash
+
         :param fast_updates: ``False`` by default; if ``True``, an alternative
                              LRU algorithm is used where all function
                              invocations except every Nth
@@ -59,7 +59,7 @@ def memoize(func=None, update_interval=300, max_size=256, skip_first=False, fast
                              you are sure that the possible number of argument
                              combinations is smaller than ``max_size`` """
 
-    # the decorator can be used with an argument as well as without any 
+    # the decorator can be used with an argument as well as without any
     if func is None:
         def wrapper(f):
             return memoize(func=f,
@@ -68,25 +68,25 @@ def memoize(func=None, update_interval=300, max_size=256, skip_first=False, fast
                            skip_first=skip_first,
                            fast_updates=fast_updates)
         return wrapper
- 
+
     cached_values = {}
 
     if fast_updates:
         lru_indices = {'CURRENT': 0}
-    else: 
+    else:
         lru_list = []
- 
+
     @wraps(func)
     def wrapper_standard(*args, **kwargs):
         if skip_first:
             key = pickle.dumps((args[1:], kwargs))
         else:
             key = pickle.dumps((args, kwargs))
- 
+
         if key in cached_values:
             # move argument to the end of the LRU list
             lru_list.append(lru_list.pop(lru_list.index(key)))
-            
+
             # get the buffered values and check whether they are up-to-date
             result, acquisition_time = cached_values[key]
             if update_interval and time() - acquisition_time > update_interval:
@@ -94,15 +94,15 @@ def memoize(func=None, update_interval=300, max_size=256, skip_first=False, fast
 
         if key not in cached_values:
             cached_values[key] = (func(*args, **kwargs), time())
-            lru_list.append(key)   
- 
-            # clear the least recently used value if the maximum size 
+            lru_list.append(key)
+
+            # clear the least recently used value if the maximum size
             # of the buffer is exceeded
             if max_size is not None and len(lru_list) > max_size:
                 del cached_values[lru_list.pop(0)]
 
         return cached_values[key][0]
-    
+
     @wraps(func)
     def wrapper_fast_updates(*args, **kwargs):
         if skip_first:
@@ -112,7 +112,7 @@ def memoize(func=None, update_interval=300, max_size=256, skip_first=False, fast
 
         lru_indices['CURRENT'] += 1
         lru_indices[key] = lru_indices['CURRENT']
- 
+
         if key in cached_values:
             # get the buffered values and check whether they are up-to-date
             result, acquisition_time = cached_values[key]
@@ -121,11 +121,11 @@ def memoize(func=None, update_interval=300, max_size=256, skip_first=False, fast
 
         if key not in cached_values:
             cached_values[key] = (func(*args, **kwargs), time())
- 
-            # clear the least recently used value if the maximum size 
+
+            # clear the least recently used value if the maximum size
             # of the buffer is exceeded
             if max_size is not None and len(lru_indices) > max_size:
-                lru_key = min(lru_indices.iteritems(), key=lambda x: x[1])[0]         
+                lru_key = min(lru_indices.iteritems(), key=lambda x: x[1])[0]
                 del lru_indices[lru_key]
                 del cached_values[lru_key]
 
@@ -137,5 +137,5 @@ def memoize(func=None, update_interval=300, max_size=256, skip_first=False, fast
                 lru_indices['CURRENT'] += 1
 
         return cached_values[key][0]
- 
+
     return wrapper_standard if not fast_updates else wrapper_fast_updates
