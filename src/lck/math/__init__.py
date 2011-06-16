2a2fc119ec310ab2168509f5f0c1da49c8839dd2
Łukasz Langa <lukasz@langa.pl>
2011-06-16 12:40:09 +0200

    Typo fix.

--- src/lck/math/__init__.py
+++ src/lck/math/__init__.py
@@ -20,8 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-"""lck.cache
-   ---------
+"""lck.math
+   --------
 
    Various math related utilities."""
 
